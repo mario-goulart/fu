c14761a2802fce79b3bdc43f892c89fe8e6c3911
Mario Domenech Goulart <mario.goulart@gmail.com>
2016-02-07 17:26:59 -0200

    oe: add initial OpenEmbedded plugin for fu

---
+++ goodies/oe/oe.scm
@@ -0,0 +1,361 @@
+;; TODO: parse local.conf to expand MACHINE and DISTRO
+;; TODO: x: -e to indicate an expression.  Example: oe x -e '${AVAR}/${BVAR}'
+
+(use data-structures extras srfi-1 srfi-13)
+
+(define +build-dir+ #f)
+(define +local-conf-file+ #f)
+(define +bitbake-data+ #f)
+
+(define (parse-bitbake-output bitbake-data-file)
+  ;; Return a list of unparsed variable context blocks
+  (let loop ((lines (read-lines bitbake-data-file))
+             (cur-var #f)
+             (blocks '())
+             (context '()))
+    (if (null? lines)
+        (reverse (cons context blocks))
+        (let ((line (car lines)))
+          (if cur-var
+              (if (string-prefix? (string-append "# $") line)
+                  ;; The end of a context block
+                  (loop lines
+                        #f
+                        (cons (cons (string->symbol cur-var)
+                                    (reverse context))
+                              blocks)
+                        '())
+                  ;; The body of a context block
+                  (loop (cdr lines)
+                        cur-var
+                        blocks
+                        (cons line context)))
+              ;; No variable context block being processed
+              (if (string-prefix? (string-append "# $") line)
+                  ;; The start of a context block
+                  (loop (cdr lines)
+                        (substring (cadr (string-split line)) 1)
+                        blocks
+                        (list line))
+                  ;; Unhandled lines
+                  (loop (cdr lines)
+                        #f
+                        blocks
+                        '())))))))
+
+(define (parse-variable-context-block var)
+  ;; Returns a pair (<pre-expansion-value> . <final-value>)
+  (let ((block (alist-ref var +bitbake-data+)))
+    (if block
+        (let loop ((lines (alist-ref var +bitbake-data+))
+                   (pre-expansion-value #f)
+                   (final-value #f))
+          (if (null? lines)
+              (cons (or pre-expansion-value final-value) final-value)
+              (let ((line (car lines)))
+                (cond
+                 ((or (string-prefix? (conc var "=") line)
+                      (string-prefix? (conc "export " var "=") line))
+                  (loop (cdr lines)
+                        pre-expansion-value
+                        (substring line
+                                   (+ 1 (string-index line
+                                                      (cut char=? <> #\=))))))
+                 ((eq? pre-expansion-value 'see-next-line)
+                  (loop (cdr lines)
+                        (string-trim (substring line 1)) ;; Remove "#" and trim
+                        final-value))
+                 ((equal? line "# pre-expansion value:")
+                  (loop (cdr lines)
+                        'see-next-line
+                        final-value))
+                 (else
+                  (loop (cdr lines)
+                        pre-expansion-value
+                        final-value))))))
+        (cons #f #f))))
+
+(define (populate-bitbake-data! #!optional recipe)
+  (unless +bitbake-data+
+    (let ((raw-data-file (create-temporary-file)))
+      (handle-exceptions exn
+        (begin
+          (delete-file* raw-data-file)
+          (exit 1))
+        (system* (sprintf "bitbake -e ~a > ~a" (or recipe "") raw-data-file)))
+      (let ((data (parse-bitbake-output raw-data-file)))
+        (delete-file raw-data-file)
+        (set! +bitbake-data+ data)))))
+
+(define (string-* str n)
+  (let loop ((n n)
+             (new-str str))
+    (cond ((fx<= n 0) "")
+          ((fx= n 1) new-str)
+          (else (loop (fx- n 1) (string-append new-str str))))))
+
+(define (indent steps)
+  (string-* "    " steps))
+
+(define (show-variable-values var pre-expansion-value final-value)
+  (print "== Pre-expansion value")
+  (printf "~a${~a} = ~a\n"
+          (indent (car pre-expansion-value)) var (cdr pre-expansion-value))
+  (print "")
+  (print "== Final value")
+  (printf "~a${~a} = ~a\n"
+          (indent (car final-value)) var (cdr final-value)))
+
+(define (show-variable-expansions var)
+  (let* ((vals (parse-variable-context-block var))
+         (pre-expansion-value (car vals))
+         (final-value (cdr vals)))
+    (if (and pre-expansion-value final-value)
+        (if (equal? pre-expansion-value final-value)
+            (show-variable-values var
+                                  (cons 0 pre-expansion-value)
+                                  (cons 0 final-value))
+            ;; FIXME: recursively expand
+            (show-variable-values var
+                                  (cons 0 pre-expansion-value)
+                                  (cons 0 final-value)))
+        (die! "No such variable: ~a" var))))
+
+(define (get-var var)
+  (with-input-from-string (cdr (parse-variable-context-block var)) read))
+
+(define get-oe-source-directories
+  (let ((dirs #f))
+    (lambda ()
+      (unless dirs
+        (let ((bblayers (get-var 'BBLAYERS)))
+          (set! dirs (string-split bblayers)))
+        dirs))))
+
+(define get-oe-sysroots-directory
+  (let ((dir #f))
+    (lambda ()
+      (unless dir
+        (let ((tmpdir (get-var 'TMPDIR)))
+          (set! dir (make-pathname tmpdir "sysroots"))))
+      dir)))
+
+(define get-oe-packages-directories
+  (let ((dirs #f))
+    (lambda ()
+      (unless dirs
+        (let ((deploy-dir (get-var 'DEPLOY_DIR))
+              (pkg-classes (map string->symbol
+                                (string-split (get-var 'PACKAGE_CLASSES))))
+              (known-pkg-classes
+               '(package_rpm package_ipk package_deb package_tar)))
+          (set! dirs
+            (if (every (lambda (pkg-class)
+                         (memq pkg-class known-pkg-classes))
+                       pkg-classes)
+                (map (lambda (pkg-class)
+                       (make-pathname deploy-dir
+                                      (substring (symbol->string pkg-class)
+                                                 8 ;; length of package_
+                                                 )))
+                     pkg-classes)
+                (list deploy-dir)))))
+      dirs)))
+
+(define program-available?
+  (let ((paths (string-split (get-environment-variable "PATH") ":")))
+    (lambda (program)
+      (let loop ((paths paths))
+        (if (null? paths)
+            #f
+            (let ((path (car paths)))
+              (or (file-exists? (make-pathname path program))
+                  (loop (cdr paths)))))))))
+
+(define (run-if-program-available program fmt . args)
+  (if (program-available? program)
+      (system* (apply sprintf (cons fmt args)))
+      (die! "ERROR: This operation requires the '~a' program to be installed."
+            program)))
+
+(define (package-view package)
+  (let ((ext (string->symbol (pathname-extension package))))
+    (case ext
+      ((deb ipk)
+       (system* (sprintf "ar p ~a data.tar.gz | tar tvzf -" package)))
+      ((rpm)
+       (run-if-program-available "rpm" "rpm -qlvp ~a" package))
+      (else (die! "ERROR: unsupported package file extension: ~a" ext)))))
+
+(define (package-info package)
+  (let ((ext (string->symbol (pathname-extension package))))
+    (case ext
+      ((deb ipk)
+       (system* (sprintf "ar p ~a control.tar.gz | tar xzf - ./control -O"
+                         package)))
+      ((rpm)
+       (run-if-program-available "rpm" "rpm -qip ~a" package))
+      (else (die! "ERROR: unsupported package file extension: ~a" ext)))))
+
+(define (package-scripts package)
+  (let ((ext (string->symbol (pathname-extension package))))
+    (case ext
+      ((deb ipk)
+       (system* (sprintf "ar p ~a control.tar.gz | tar tzf -" package)))
+      (else (die! "ERROR: unsupported package file extension: ~a" ext)))))
+
+(define (package-extract package)
+  (let-values (((dir out-dir ext) (decompose-pathname package)))
+    (if (file-exists? out-dir)
+        (die! "~a already exists.  Won't clobber.  Aborting." out-dir)
+        (create-directory out-dir 'recursively))
+    (case (string->symbol ext)
+      ((deb ipk)
+       (system* "ar p data.tar.gz ~a | tar xzf - -C ~a" package out-dir))
+      ((rpm)
+       (change-directory out-dir)
+       (run-if-program-available "rpm2cpio ~a | cpio -id" package))
+      (else
+       (die! "ERROR: unsupported package file extension: ~a" ext)))))
+
+(define (package-actions)
+  (lambda (selection)
+    (let ((labels/actions `(("View"    . ,package-view)
+                            ("Info"    . ,package-info)
+                            ("Scripts" . ,package-scripts)
+                            ("Extract" . ,package-extract))))
+      (when (terminal-port? (current-output-port))
+        (print-selected-file selection)
+        (let ((option (prompt (map car labels/actions) identity)))
+          ((cdr (list-ref labels/actions option)) selection))))))
+
+
+(define oe-usage
+  "Usage: oe <command> <options>
+
+<command>s:
+
+f [-s] [-f] [-d <depth>] <pattern>
+  Find files that sloppily match <pattern> (a regular expression).
+  Sloppily means <pattern> will be surrounded by \".*\" and will be
+  case insensitive.
+    -s:          strict mode -- disable sloppy mode.
+    -e <except>: remove files matching <except> (not affected by -s)
+    -f:          print full paths
+    -d <depth>:  limit search to <depth>
+    -.        :  list files whose name start with \".\"
+
+v <f options> [<dir>] <pattern>
+  Find files & view.
+
+e <f options> [<dir>] <pattern>
+  Find files & edit.
+
+x [-s] [-u] <variable> [<recipe>]
+  Expand <variable>.  If <recipe> is provided, expand <variable> in the
+  context of <recipe>.  If -s (short) is provided, only the final value
+  will be printed.  If -u is provided, the raw output of 'bitbake -e'
+  for the variable in question will be printed.
+")
+
+(define-command 'oe
+  "oe <options>
+   Run 'oe help' for the full help message."
+  (lambda args
+    (when (null? args)
+      (die! oe-usage))
+
+    (let ((builddir (get-environment-variable "BUILDDIR")))
+      (unless builddir
+        (die! "The build environment is not set.  Aborting."))
+      (set! +build-dir+ builddir)
+      (set! +local-conf-file+
+        (make-pathname (list +build-dir+ "conf") "local.conf")))
+
+    (let ((cmd (string->symbol (car args)))
+          (oe-args (cdr args)))
+      (case cmd
+        ((h help)
+         (print oe-usage)
+         (exit))
+
+        ((x)
+         (if (null? oe-args)
+             (die! "Missing variable.  Aborting.")
+             (let* ((non-options (remove (lambda (arg)
+                                           (string-prefix? "-" arg))
+                                         oe-args))
+                    (var (string->symbol (car non-options)))
+                    (recipe (and (not (null? (cdr non-options)))
+                                 (cadr non-options))))
+               (populate-bitbake-data! recipe)
+               (cond ((member "-s" oe-args)
+                      (print (cdr (parse-variable-context-block var))))
+                     ((member "-u" oe-args)
+                      (for-each print (alist-ref var +bitbake-data+)))
+                     (else
+                      (show-variable-expansions var))))))
+
+        ((f e v sf se sv)
+         (populate-bitbake-data!)
+         (apply (fu-find/operate (case cmd
+                                   ((f sf) (fu-actions))
+                                   ((e se) (fu-editor))
+                                   ((v sv) (fu-viewer)))
+                                 dir: (case cmd
+                                        ((f e v) (get-oe-source-directories))
+                                        ((sf se sv) (get-oe-sysroots-directory))))
+                (cdr args)))
+
+        ((pf pv pi ps px)
+         (populate-bitbake-data!)
+         (apply (fu-find/operate (case cmd
+                                   ((pf) (package-actions))
+                                   ((pv) package-view)
+                                   ((pi) package-info)
+                                   ((ps) package-scripts)
+                                   ((px) package-extract))
+                                 dir: (get-oe-packages-directories))
+                (cdr args)))
+
+        ((l)
+         (if (null? oe-args)
+             (die! "Missing recipe.  Aborting.")
+             (let* ((non-options (remove (lambda (arg)
+                                           (string-prefix? "-" arg))
+                                         oe-args))
+                    (recipe (string->symbol (car non-options)))
+                    (log (and (not (null? (cdr non-options)))
+                              (cadr non-options))))
+               (populate-bitbake-data! recipe)
+               (let ((log-pattern
+                      (prepare-pattern
+                       (if log
+                           (string-append "log\\.do_" log)
+                           "log\\..*")
+                       #f)))
+                 (maybe-prompt-files
+                  (filter symbolic-link?
+                          (fu-find-files log-pattern
+                                         dir: (make-pathname (get-var 'WORKDIR)
+                                                             "temp")))
+                  (if log log-pattern ".*")
+                  (fu-viewer)
+                  pre-formatter: pathname-strip-directory)))))
+
+        ((r)
+         (die! "FIXME"))
+
+        ((gv)
+         (die! "FIXME"))
+
+        ((ge)
+         (die! "FIXME"))
+
+        ((d)
+         (die! "FIXME"))
+
+        ((cd)
+         (die! "FIXME"))
+
+        (else (die! "Unknown command: ~a" cmd))))))
---
+++ goodies/oe/oe.sh
@@ -0,0 +1,34 @@
+oe () {
+    if [ -z "$1" ]; then
+        fu oe help
+    elif [ "$1" == "cd" ]; then
+        local where=$2
+        if [ -z "$where" ]; then
+            cd $BUILDDIR
+            return 0
+        fi
+        local dir
+        case "$where" in
+            top)
+                if [ -z "$BUILDDIR" ]; then
+                    echo "BUILDDIR is not set" >&2
+                    return 1
+                else
+                    dir=$BUILDDIR  
+                fi
+                ;;
+            bh) dir=`fu oe x -s BUILDHISTORY_DIR` ;;
+            pkg) dir=`fu oe x -s DEPLOY_DIR` ;;
+            *) dir=
+        esac
+        if [ -z "$dir" ]; then
+            echo "Unknown location specifier: $where" >&2
+            return 1
+        fi
+        # oe prints the value quoted, so we remove quotes here
+        dir=`echo $dir | sed 's/^"//; s/"$//'`
+        cd $dir
+    else
+        fu oe "$@"
+    fi
+}
