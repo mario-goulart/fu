d6308e8c5826bdbd851778c51068a67ecb205073
Mario Domenech Goulart <mario.goulart@gmail.com>
2016-02-14 18:05:59 +0100

    goodies/oe/oe.scm: use *var* notation instead of +var+, for consistency

--- goodies/oe/oe.scm
+++ goodies/oe/oe.scm
@@ -4,13 +4,13 @@
 (use data-structures extras files ports posix srfi-1 srfi-13 utils)
 (use html-parser sxml-transforms)
 
-(define +build-dir+ #f)
-(define +local-conf-file+ #f)
-(define +bitbake-data+ #f)
-(define +fu-oe-data-dir+ #f)
-(define +cached-oe-data-file+ #f)
-(define +tracked-config-files+ #f)
-(define +cached-oe-variables+
+(define *build-dir* #f)
+(define *local-conf-file* #f)
+(define *bitbake-data* #f)
+(define *fu-oe-data-dir* #f)
+(define *cached-oe-data-file* #f)
+(define *tracked-config-files* #f)
+(define *cached-oe-variables*
   '(DEPLOY_DIR BBLAYERS TMPDIR PACKAGE_CLASSES))
 (define *documentation-cache-file* #f)
 
@@ -62,9 +62,9 @@
 
 (define (parse-variable-context-block var)
   ;; Returns a pair (<pre-expansion-value> . <final-value>)
-  (let ((block (alist-ref var +bitbake-data+)))
+  (let ((block (alist-ref var *bitbake-data*)))
     (if block
-        (let loop ((lines (alist-ref var +bitbake-data+))
+        (let loop ((lines (alist-ref var *bitbake-data*))
                    (pre-expansion-value #f)
                    (final-value #f))
           (if (null? lines)
@@ -93,22 +93,22 @@
         (cons #f #f))))
 
 (define (populate-bitbake-data-from-cache!)
-  (set! +bitbake-data+ (read-file +cached-oe-data-file+)))
+  (set! *bitbake-data* (read-file *cached-oe-data-file*)))
 
 (define (populate-bitbake-data! #!optional recipe)
-  (unless +bitbake-data+
+  (unless *bitbake-data*
     (let ((raw-data-file (create-temporary-file))
           (anchor (current-directory)))
       (handle-exceptions exn
         (begin
           (delete-file* raw-data-file)
           (exit 1))
-        (change-directory +build-dir+)
+        (change-directory *build-dir*)
         (system* "bitbake -e ~a > ~a" (or recipe "") raw-data-file)
         (change-directory anchor))
       (let ((data (parse-bitbake-output raw-data-file)))
         (delete-file raw-data-file)
-        (set! +bitbake-data+ data)))))
+        (set! *bitbake-data* data)))))
 
 (define (string-* str n)
   (let loop ((n n)
@@ -278,44 +278,44 @@
   (handle-exceptions exn
     #t
     (let ((stored-sums
-           (read-file (make-pathname +fu-oe-data-dir+ "config-sums.scm")))
+           (read-file (make-pathname *fu-oe-data-dir* "config-sums.scm")))
           (actual-sums
            (map (lambda (file)
                   (cons file
-                        (sha256-sum (make-pathname (list +build-dir+ "conf")
+                        (sha256-sum (make-pathname (list *build-dir* "conf")
                                                    file))))
-                +tracked-config-files+)))
+                *tracked-config-files*)))
       (any (lambda (file)
              (not (equal? (alist-ref file actual-sums equal?)
                           (alist-ref file stored-sums equal?))))
-           +tracked-config-files+))))
+           *tracked-config-files*))))
 
 (define (write-config-sums!)
-  (with-output-to-file (make-pathname +fu-oe-data-dir+ "config-sums.scm")
+  (with-output-to-file (make-pathname *fu-oe-data-dir* "config-sums.scm")
     (lambda ()
       (for-each
        (lambda (file)
          (pp (cons file
-                   (sha256-sum (make-pathname (list +build-dir+ "conf")
+                   (sha256-sum (make-pathname (list *build-dir* "conf")
                                               file)))))
-       +tracked-config-files+))))
+       *tracked-config-files*))))
 
 (define (write-basic-oe-data!)
   (debug 2 "Writing basic OE data")
   (populate-bitbake-data!)
-  (with-output-to-file +cached-oe-data-file+
+  (with-output-to-file *cached-oe-data-file*
     (lambda ()
       (for-each (lambda (var)
                   (pp (cons var
-                            (cdr (alist-ref var +bitbake-data+)))))
-                +cached-oe-variables+)))
+                            (cdr (alist-ref var *bitbake-data*)))))
+                *cached-oe-variables*)))
   ;; Maybe bitbake must have to be rerun in a recipe-specific basis,
   ;; so we just empty the in-memory data
-  (set! +bitbake-data+ #f))
+  (set! *bitbake-data* #f))
 
 (define (maybe-store-basic-oe-data!)
   (when (or (config-changed?)
-            (not (file-exists? +cached-oe-data-file+)))
+            (not (file-exists? *cached-oe-data-file*)))
     (debug 0 "Configuration files have changed.  Updating sums and cache...")
     (write-config-sums!)
     (write-basic-oe-data!)))
@@ -365,7 +365,7 @@
                (for-each print options))))))
 
 (define (find-oe-variable str-pattern)
-  (let* ((vars (map (compose symbol->string car) +bitbake-data+))
+  (let* ((vars (map (compose symbol->string car) *bitbake-data*))
          (pattern (prepare-pattern str-pattern #f))
          (results (filter (lambda (var)
                             (irregex-search pattern var))
@@ -669,21 +669,21 @@ run
     (let ((builddir (get-environment-variable "BUILDDIR")))
       (unless builddir
         (die! "The build environment is not set.  Aborting."))
-      (set! +build-dir+ builddir)
-      (set! +local-conf-file+
-        (make-pathname (list +build-dir+ "conf") "local.conf"))
-      (set! +fu-oe-data-dir+ (make-pathname builddir ".fu-oe"))
-      (set! +cached-oe-data-file+ (make-pathname +fu-oe-data-dir+
+      (set! *build-dir* builddir)
+      (set! *local-conf-file*
+        (make-pathname (list *build-dir* "conf") "local.conf"))
+      (set! *fu-oe-data-dir* (make-pathname builddir ".fu-oe"))
+      (set! *cached-oe-data-file* (make-pathname *fu-oe-data-dir*
                                                  "cached-variables.scm"))
-      (set! +tracked-config-files+
+      (set! *tracked-config-files*
         (filter (lambda (file)
-                  (file-exists? (make-pathname (list +build-dir+ "conf")
+                  (file-exists? (make-pathname (list *build-dir* "conf")
                                                file)))
                 '("local.conf" "bblayers.conf" "site.conf")))
       (set! *documentation-cache-file*
-        (make-pathname +fu-oe-data-dir+ "cached-documentation.scm")))
+        (make-pathname *fu-oe-data-dir* "cached-documentation.scm")))
 
-    (create-directory +fu-oe-data-dir+ 'recursively)
+    (create-directory *fu-oe-data-dir* 'recursively)
     (maybe-store-basic-oe-data!)
 
     (let ((cmd (string->symbol (car args)))
@@ -706,7 +706,7 @@ run
                (cond ((member "-s" oe-args)
                       (print (cdr (parse-variable-context-block var))))
                      ((member "-u" oe-args)
-                      (for-each print (alist-ref var +bitbake-data+)))
+                      (for-each print (alist-ref var *bitbake-data*)))
                      (else
                       (show-variable-expansions var))))))
 
